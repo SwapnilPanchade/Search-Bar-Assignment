cb8019067ba7a6600533221dc8dbed244611cc8b
SwapnilPanchade <sspanchade113@gmail.com>
2025-03-28 14:27:55 +0530

    Assignment Completion

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Searchbar } from "./components/Searchbar";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div className="search-bar-container">
+          <Searchbar />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FaSearch, FaArrowRight, FaTimes } from "react-icons/fa";
+
+import "./Searchbar.css";
+
+export const Searchbar = () => {
+  const [input, setInput] = useState("");
+  const searchRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (searchRef.current && !searchRef.current.contains(event.target)) {
+        setInput((prev) => prev);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="input-wrapper" ref={searchRef}>
+      {input ? (
+        <FaArrowRight id="search-icon" />
+      ) : (
+        <FaSearch id="search-icon" />
+      )}
+      <input
+        type="text"
+        placeholder="Type to search..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      {input && (
+        <div className="clear-wrapper">
+          <span className="clear-text" onClick={() => setInput("")}>
+            Clear
+          </span>
+          <FaTimes id="clear-icon" onClick={() => setInput("")} />
+        </div>
+      )}
+    </div>
+  );
+};
